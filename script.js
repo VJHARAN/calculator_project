00f370270f69166ff23da4e3767d0a65f749133e
VJHARAN <haranvj1997@gmail.com>
2025-04-09 18:14:34 +0530

    Create function to populate display on  button click

--- script.js
+++ script.js
@@ -32,17 +32,77 @@ function operate(operator,num1, num2){
     }
 }
 
- 
-const expression=prompt("Input expression: ");
-// console.log(expression);
-
-let input='';
-for (item of expression){
-    if (!isNaN(item))
-        input+=item;
-    else 
-     input +=' '+item+' ';
+function populateDisplay(inputVal){
+    const display=document.querySelector("input#display");
+    // console.log(display);
+    display.value+=inputVal;
+    
 }
+
+
+
+
+
+
+
+let buttons=document.querySelectorAll('.input-button button');
+buttons=Array.from(buttons);
+
+buttons.filter(item=> !item.id).forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        let inputVal=e.target.textContent;
+        // console.log(inputVal);
+
+        populateDisplay(inputVal);
+
+        expression+=e.target.textContent;  
+        // console.log(expression);
+        for (item of expression){
+            if (!isNaN(item))
+                input+=item;
+            else 
+             input +=' '+item+' ';
+        }
+    }
+    
+    );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const expression=prompt("Input expression: ");
+// console.log(expression);
+let expression='';
+let input='';
+// for (item of expression){
+//     if (!isNaN(item))
+//         input+=item;
+//     else 
+//      input +=' '+item+' ';
+// }
 // console.log(input);
 
 finalExpression=input.split(' ').map(item=>isNaN(item)?item:+item);
