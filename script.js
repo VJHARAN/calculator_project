654c05e3fb7b02ed7b2bf0e70739a08dac7ea9b9
VJHARAN <haranvj1997@gmail.com>
2025-04-09 16:46:09 +0530

    Add operate function

--- script.js
+++ script.js
@@ -19,3 +19,35 @@ function product(num1,num2){
     console.log("Product= ",prod);
 }
 
+function operate(operator,num1, num2){
+    switch(operator){
+        case '+': sum(num1,num2);
+                    break;
+        case '-': difference(num1,num2);
+                    break;
+        case '/': divide(num1,num2);
+                    break;
+        case '*': product(num1,num2);
+                    break;
+    }
+}
+
+ 
+const expression=prompt("Input expression: ");
+// console.log(expression);
+
+let input='';
+for (item of expression){
+    if (!isNaN(item))
+        input+=item;
+    else 
+     input +=' '+item+' ';
+}
+// console.log(input);
+
+finalExpression=input.split(' ').map(item=>isNaN(item)?item:+item);
+const [num1,operator,num2]=finalExpression;
+
+// console.log(typeof num1, typeof num2, typeof operator);
+
+operate(operator,num1,num2);
