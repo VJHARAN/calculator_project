a386a2e811dc5c3255eeb8f8ba3171e5402a1ceb
VJHARAN <haranvj1997@gmail.com>
2025-04-09 16:44:25 +0530

    Create function for each operation

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function sum(num1,num2){
+    total= num1+num2;
+    console.log("Sum= ",total);
+}
+
+function difference(num1,num2){
+    diff= num1-num2;
+    console.log("Difference= ",diff);
+}
+
+function divide(num1,num2){
+    quotient= num1/num2;
+    console.log("Quotient= ",quotient);
+}
+
+function product(num1,num2){
+    prod=num1*num2;
+    console.log("Product= ",prod);
+}
+
